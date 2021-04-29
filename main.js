a04336d09308ae63fbb7ef93f29158986ddd672c
bicitrobiggan <68156215+bicitrobiggan@users.noreply.github.com>
2021-04-29 12:35:23 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+  setTimeout(loader,5000);
+}
+function loader(){
+document.getElementById("loader").style.display = "none";
+document.getElementById("body").style.display = "block";
+}
